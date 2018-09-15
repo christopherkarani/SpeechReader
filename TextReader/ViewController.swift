4f711f24708484178da3a9167a4696a45c0d9163
Chris Karani <christopher.karani@incentro.com>
2018-09-15 22:49:46 +0300

    refactoring view code from View controller

--- TextReader/ViewController.swift
+++ TextReader/ViewController.swift
@@ -14,124 +14,21 @@ import Spring
 
 
 
+
+
 class ViewController: UIViewController {
     
-
-    
-    let synthesizer = AVSpeechSynthesizer()
-    
-    
-    let inputTextField: SpringTextField = {
-        let tf = SpringTextField()
-        tf.placeholder = "Enter Some Text"
-        tf.borderStyle = .roundedRect
-        tf.backgroundColor = .white
-        return tf
-    }()
-    
-    
-    let readButton : SpringButton = {
-        let button = SpringButton(type: .system)
-        button.setTitle("Read Text", for: .normal)
-        button.backgroundColor = UIColor.flatGreen
-        button.layer.cornerRadius = 5
-        button.setTitleColor(.white, for: .normal)
-        button.addTarget(self, action: #selector(speakAction), for: .touchUpInside)
-        button.shadow(with: .flatGray, opacity: 0.7, offset: CGSize(width: 0.5, height: 5), radius: 2)
-        return  button
-    }()
-    
-    let repeatButton: SpringButton = {
-        let button = SpringButton(type: .system)
-        button.setTitle("Repeat", for: .normal)
-        button.backgroundColor = .gray
-        button.layer.cornerRadius = 5
-        button.setTitleColor(.white, for: .normal)
-        button.addTarget(self, action: #selector(repeatSpeechAction), for: .touchUpInside)
-        button.shadow(with: .flatGray, opacity: 0.7, offset: CGSize(width: 0.5, height: 5), radius: 2)
-        return  button
+    var textReaderInputView: TextReaderInputView = {
+        let view = TextReaderInputView(with: AVSpeechSynthesizer())
+        return view
     }()
     
     let commonWordsContainerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .clear
         return view
     }()
     
-
-    
-    var repeatText: String?
-    
-    
-    // handle repeat action
-    @objc func repeatSpeechAction() {
-        guard let text = repeatText else { return }
-        animate(button: .repeat)
-        let utterance = AVSpeechUtterance(string: text)
-        let voices : Voices = .english
-        utterance.voice = AVSpeechSynthesisVoice(language: voices.language)
-        if !synthesizer.isSpeaking {
-            synthesizer.speak(utterance)
-        }
-    }
-    
-    fileprivate func handleTextFieldErrorAnimation() {
-        inputTextField.animation = "shake"
-        inputTextField.curve = "spring"
-        inputTextField.duration = 0.5
-        inputTextField.animate()
-    }
-    
-    /*
-     a helper enum for button selection
- */
-    
-    enum ButtonType {
-        case read, `repeat`
-    }
-    
-    fileprivate func animate(button type: ButtonType) {
-        switch type {
-        case .read:
-            readButton.scaleX = 0.90
-            readButton.scaleY = 0.90
-            readButton.duration = 1
-            readButton.animate()
-        case .repeat:
-            repeatButton.scaleX = 0.90
-            repeatButton.scaleY = 0.90
-            repeatButton.duration = 1
-            repeatButton.animate()
-        }
-    }
-    
-    
-    // handle speaking
-    @objc func speakAction() {
-        guard let text = inputTextField.text, !text.isEmpty else {
-            handleTextFieldErrorAnimation()
-            return
-        }
-        
-        animate(button: .read)
-        
-        
-        let utterance = AVSpeechUtterance(string: text)
-        let voices : Voices = .english
-        utterance.voice = AVSpeechSynthesisVoice(language: voices.language)
-        synthesizer.speak(utterance)
-        repeatText = text
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.repeatButton.backgroundColor = UIColor.flatRed
-            self.repeatButton.setTitleColor(.white, for: .normal)
-        }
-        
-        inputTextField.text = nil
-        
-
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         initialUISetup()
@@ -164,44 +61,31 @@ class ViewController: UIViewController {
     }
     
     private func setupUIElements() {
-        view.addSubview(inputTextField)
-        view.addSubview(readButton)
-        view.addSubview(repeatButton)
-        view.addSubview(commonWordsContainerView)
+        view.addSubview(textReaderInputView)
+
+//        view.addSubview(commonWordsContainerView)
         
         
         
-        // input textFIeld contrainsts
-        inputTextField.snp.makeConstraints {
+        // input view contrainsts
+        textReaderInputView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.centerY.equalToSuperview().offset(-50)
             $0.width.equalToSuperview().inset(14)
             $0.height.equalTo(50)
         }
         
-        // read button constraints
-        readButton.snp.makeConstraints {
-            $0.top.equalTo(inputTextField.snp.bottom).offset(20)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(inputTextField.snp.width).multipliedBy(1.5/3)
-            $0.height.equalTo(40)
-        }
         
-        // repeat button constraints
-        repeatButton.snp.makeConstraints {
-            $0.top.equalTo(readButton.snp.bottom).offset(10)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(inputTextField.snp.width).multipliedBy(1.5/3)
-            $0.height.equalTo(40)
-        }
         
-        // common words container view constraints
-        commonWordsContainerView.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(repeatButton.snp.bottom).offset(16)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
-            $0.width.equalToSuperview().inset(14)
-        }
+        
+//
+//        // common words container view constraints
+//        commonWordsContainerView.snp.makeConstraints {
+//            $0.centerX.equalToSuperview()
+//            $0.top.equalTo(repeatButton.snp.bottom).offset(16)
+//            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+//            $0.width.equalToSuperview().inset(14)
+//        }
         
     }
 
---
+++ TextReader/Views/TextReaderInputView.swift
@@ -0,0 +1,158 @@
+
+//
+//  File.swift
+//  TextReader
+//
+//  Created by Chris Karani on 15/09/2018.
+//  Copyright © 2018 Chris Karani. All rights reserved.
+//
+
+import UIKit
+import Spring
+import AVFoundation
+import SnapKit
+
+final class TextReaderInputView: UIView {
+    
+    var synthesizer: AVSpeechSynthesizer
+    var repeatText: String?
+    
+    
+    let inputTextField: SpringTextField = {
+        let tf = SpringTextField()
+        tf.placeholder = "Enter Some Text"
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = .white
+        return tf
+    }()
+    
+    
+    public lazy var readButton : SpringButton = {
+        let button = SpringButton(type: .system)
+        button.setTitle("Read Text", for: .normal)
+        button.backgroundColor = UIColor.flatGreen
+        button.layer.cornerRadius = 5
+        button.setTitleColor(.white, for: .normal)
+        //button.addTarget(self, action: #selector(actions.read), for: .touchUpInside)
+        button.shadow(with: .flatGray, opacity: 0.7, offset: CGSize(width: 0.5, height: 5), radius: 2)
+        return  button
+    }()
+    
+    public lazy var repeatButton: SpringButton = {
+        let button = SpringButton(type: .system)
+        button.setTitle("Repeat", for: .normal)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 5
+        button.setTitleColor(.white, for: .normal)
+        //button.addTarget(self, action: #selector(actions.repeat), for: .touchUpInside)
+        button.shadow(with: .flatGray, opacity: 0.7, offset: CGSize(width: 0.5, height: 5), radius: 2)
+        return  button
+    }()
+    
+    lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [readButton, repeatButton])
+        stackView.backgroundColor = .yellow
+        stackView.spacing = 5
+        stackView.distribution = .fillEqually
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    
+    
+    
+    init(with synthesizer: AVSpeechSynthesizer) {
+        self.synthesizer = synthesizer
+        
+        super.init(frame: .zero)
+        
+        setupUI()
+    }
+    
+    fileprivate func setupUI() {
+        addSubview(buttonsStackView)
+        
+        buttonsStackView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(inputTextField.snp.bottom).offset(20)
+            $0.width.equalTo(inputTextField.snp.width)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    /*
+     a helper enum for button selection
+     */
+    
+    enum ButtonType {
+        case read, `repeat`
+    }
+    
+    fileprivate func animate(button type: ButtonType) {
+        switch type {
+        case .read:
+            readButton.scaleX = 0.90
+            readButton.scaleY = 0.90
+            readButton.duration = 1
+            readButton.animate()
+        case .repeat:
+            repeatButton.scaleX = 0.90
+            repeatButton.scaleY = 0.90
+            repeatButton.duration = 1
+            repeatButton.animate()
+        }
+    }
+    
+    
+    // error animation
+    fileprivate func handleTextFieldErrorAnimation() {
+        inputTextField.animation = "shake"
+        inputTextField.curve = "spring"
+        inputTextField.duration = 0.5
+        inputTextField.animate()
+    }
+    
+    // handle repeat action
+    @objc func repeatSpeechAction() {
+        guard let text = repeatText else { return }
+        // press down animation
+        animate(button: .repeat)
+        let utterance = AVSpeechUtterance(string: text)
+        let voices : Voices = .english
+        utterance.voice = AVSpeechSynthesisVoice(language: voices.language)
+        if !synthesizer.isSpeaking {
+            synthesizer.speak(utterance)
+        }
+    }
+    
+    // handle speaking
+    @objc func speakAction() {
+        guard let text = inputTextField.text, !text.isEmpty else {
+            handleTextFieldErrorAnimation()
+            return
+        }
+        
+        
+        // press down animation
+        animate(button: .read)
+        
+        let utterance = AVSpeechUtterance(string: text)
+        let voices : Voices = .english
+        utterance.voice = AVSpeechSynthesisVoice(language: voices.language)
+        synthesizer.speak(utterance)
+        repeatText = text
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.repeatButton.backgroundColor = UIColor.flatRed
+            self.repeatButton.setTitleColor(.white, for: .normal)
+        }
+        
+        inputTextField.text = nil
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
